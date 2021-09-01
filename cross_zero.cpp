a4c3d865f9e2cb8bbd3d5eeb1793581f1acb5cd7
Yuri <69256174+kyurik5010@users.noreply.github.com>
2021-09-02 00:00:33 +0300

    Add files via upload

---
+++ cross_zero.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+void task1()
+{
+  bool a0 = false;
+  short int a1 = 3;
+  char a2 = "k";
+  int a = 1;
+  signed int b = -1;
+  unsigned int c = 2;
+  float d = 3.7f;
+  double e = 3.7874539874;
+}
+
+
+
+int main()
+{
+    enum objects{
+        inputCross,
+        inputZero,
+        emptySpace,
+    };
+    
+    struct gameSet{
+        struct playField{
+            char a[4] = {' ',' ',' ',0};
+            char b[4] = {' ',' ',' ',0};
+            char c[4] = {' ',' ',' ',0};
+        };
+        char player1[30] = {0};
+        char player2[30] = {0};
+        char player1Vict[40] = {0};
+        char player2Vict[40] = {0};
+        char draw[40] = {0};
+    };
+    
+    /*int i,k;
+    char arr[3][4]= {{'_','_','_'},{'_','_','_'},{'_','_','_'}};
+    for(i=0;i<=2;i++)
+    {
+        for(k=0;k<=2;k++)
+        {
+        cout<< arr[i][k] << "|";
+        }
+        cout<<endl;
+    }*/
+    
+    cout << "everything seems to be fine\n";
+    cout << "size of structure - " << sizeof(gameSet) << endl;
+    cout << "size of enum - " << sizeof(objects) << endl;
+    return 0;
+}
