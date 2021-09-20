103d24dae6fc80f2351bffd3cd8d998f52985f49
kyurik5010 <ykalintsev338@mail.ru>
2021-09-21 00:33:12 +0300

    added lesson_7

---
+++ lesson_7/mylib.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+
+namespace msp {
+    void init(float arr[], size_t size);
+
+    void printarr(float arr[], size_t size);
+
+    int checksum(float arr[], size_t size);
+}
