db710fa9f75112df564c5dad35c5c6e26ac7b3cc
Yuri <69256174+kyurik5010@users.noreply.github.com>
2021-11-24 00:09:19 +0300

    update 24/11/2021

--- algorithms/lesson_7/main.cpp
+++ algorithms/lesson_7/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
-#define SIZE 10
+#define SIZE 49
 
 void print_ar(int *arr, int);
 void auto_init(int *arr, int len, int **parr, int len2, int range);
@@ -12,7 +12,7 @@ int main() {
     int *arr = new (std::nothrow) int [SIZE];
     if(!arr) {std::cout << "Memory fault";}
     auto_init(arr, SIZE,nullptr,0,98);
-//    int arr[] = {7,10,2,4,8,5,3,9,1,6};
+//    int arr[] = {7,10,2,4,8,5,3,9,1,6,15,13,17,11,12,14,16}; // весь закоменченный код - дебаг (штатный на маке не работает)
     std::cout << "original array: ";
     print_ar(arr, SIZE);
     int first = 0;
@@ -24,10 +24,9 @@ int main() {
 
 void Qsort(int *arr, int first, int last)
 {
-//    std::cout << "\n\nstarting quick sort";
-     // arr[4] - 8
-    if (last+1 <= 10)
+    if ((last-first) <= 10)
     {
+//        std::cout<<"\n\nstarting insert sort";
         int tmp,current;
         for(int min=1, len=last+1;min<len;++min)
         {
@@ -42,32 +41,44 @@ void Qsort(int *arr, int first, int last)
         }
     }
     else {
-//    printf("\nfirst-%d pivot-%d last-%d", arr[F], P, arr[L]);
-        
-            int F = first; 
-            int L = last; 
-            int P = (last+1)/2;
-            // определить медиану
-         
-            if((F>P)&&(F<L))
-                swap(&arr[F],&arr[P]);
-            else if((L>F)&&(L<P))
-                swap(&arr[L],&arr[P]);
-     do{
-//          std::cout << "\nruning cycle " << "F-" << F << " L-" << L;
-            while (arr[F] < P) { ++F; }
-            while (arr[L] > P) { --L; }
+        int F = first;
+        int L = last;
+        int P = last-1; 
+
+//        printf("\n\ninitial points - F/arr[%d]/%d P/arr[%d]/%d L/arr[%d]/%d",F, arr[F], P, arr[P], L, arr[L]);
+
+        if((arr[F]>arr[P])&&(arr[F]<arr[L])) {
+            swap(&arr[F], &arr[P]);
+//            std::cout << "\nredefined pivot:";
+//            std::cout << " F/arr["<<F<<"]/"<< arr[F] << " P/arr["<<P<<"]/"<<arr[P]<<" L/arr["<<L<<"]/"<<arr[L];
+//            print_ar(arr,SIZE);
+        }
+        else if((arr[L]>arr[F])&&(arr[L]<arr[P])) {
+            swap(&arr[L], &arr[P]);
+//            std::cout << "\nredefined pivot:";
+//            std::cout << "\nF-" << arr[F] << " P-" << arr[P] <<" L-" << arr[L];
+//            print_ar(arr,SIZE);
+        }
+        else
+//            std::cout << "\n\npivot remains";
+        do{
+//            std::cout << "\nruning cycle " << " F/arr[" << F << "]/" <<arr[F]<< " L/arr[" << L<<"]/"<<arr[L];
+            while (arr[F] < arr[P]) { ++F; }
+            while (arr[L] > arr[P]) { --L; }
+//            std::cout << "\nstop: F/arr["<<F<<"]/"<<arr[F]<<" > P/arr["<<P<<"]/"<<arr[P]<<" > L/arr["<<L<<"]/"<<arr[L];
             if (arr[F] >= arr[L]) {
-//            std::cout << "\nswap " << arr[F] << " <-> " << arr[L];
+//                std::cout << "\nswapping F/arr["<<F<<"]/"<<arr[F]<<" <-> L/arr["<<L<<"]/"<< arr[L];
                 swap(&arr[F], &arr[L]);
-//            std::cout << "\narr[F] - " << arr[F] << " arr[L] - " << arr[L];
+//                std::cout << "\n swapped F/arr["<<F<<"]-"<<arr[F]<<"     L/arr["<<L<<"]-"<<arr[L];
+//                print_ar(arr,SIZE);
                 F++;
                 L--;
-//            std::cout << "\nF++ - " << arr[F] << " L-- - " << arr[L];
+
+//                std::cout << "\nF++ -> arr["<<F<<"]/"<<arr[F]<<"\nL-- -> arr["<<L<<"]/"<<arr[L];
             }
         } while (arr[F] < arr[L]);
 
-//    printf("\nPartition stop -> first-%d pivot-%d last-%d", arr[F], P, arr[L]);
+//    printf("\nPartition stop -> first-%d pivot-%d last-%d", arr[F], arr[P], arr[L]);
 //    print_ar(arr,SIZE);
         if (F < last)
             Qsort(arr, F, last);
