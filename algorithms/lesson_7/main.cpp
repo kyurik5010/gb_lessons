b708ff52c14734455f2aabe94e78c39a2078a662
kyurik5010 <ykalintsev338@mail.ru>
2021-11-28 16:49:16 +0300

    updated algorithms dir

--- algorithms/lesson_7/main.cpp
+++ algorithms/lesson_7/main.cpp
@@ -1,24 +1,39 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
-#define SIZE 49
 
+
+void Qsort(int *arr, int first, int last); // задание 1
+void Bsort(int *arr, int len); // задание 2
+//мои функции
 void print_ar(int *arr, int);
 void auto_init(int *arr, int len, int **parr, int len2, int range);
-void Qsort(int *arr, int first, int last);
 void swap(int *a, int *b);
 
 int main() {
+    int SIZE = 10;
     int *arr = new (std::nothrow) int [SIZE];
     if(!arr) {std::cout << "Memory fault";}
     auto_init(arr, SIZE,nullptr,0,98);
-//    int arr[] = {7,10,2,4,8,5,3,9,1,6,15,13,17,11,12,14,16}; // весь закоменченный код - дебаг (штатный на маке не работает)
-    std::cout << "original array: ";
+
     print_ar(arr, SIZE);
     int first = 0;
     int last = SIZE-1;
     Qsort(arr, first, last);
     print_ar(arr, SIZE);
+    delete[] arr;
+
+    std::cout << "\n\nЗадание 2";
+    SIZE = 20;
+    int *arr2 = new (std::nothrow) int [SIZE];
+    if(!arr2){
+        std::cout << "Memory fault";}
+    auto_init(arr2, SIZE,nullptr,0,98);
+    print_ar(arr2, SIZE);
+
+    Bsort(arr2, SIZE);
+    print_ar(arr2, SIZE);
+    delete[] arr2;
     return 0;
 }
 
@@ -26,7 +41,6 @@ void Qsort(int *arr, int first, int last)
 {
     if ((last-first) <= 10)
     {
-//        std::cout<<"\n\nstarting insert sort";
         int tmp,current;
         for(int min=1, len=last+1;min<len;++min)
         {
@@ -43,46 +57,69 @@ void Qsort(int *arr, int first, int last)
     else {
         int F = first;
         int L = last;
-        int P = last-1; 
-
-//        printf("\n\ninitial points - F/arr[%d]/%d P/arr[%d]/%d L/arr[%d]/%d",F, arr[F], P, arr[P], L, arr[L]);
+        int P = last-1;
 
         if((arr[F]>arr[P])&&(arr[F]<arr[L])) {
             swap(&arr[F], &arr[P]);
-//            std::cout << "\nredefined pivot:";
-//            std::cout << " F/arr["<<F<<"]/"<< arr[F] << " P/arr["<<P<<"]/"<<arr[P]<<" L/arr["<<L<<"]/"<<arr[L];
-//            print_ar(arr,SIZE);
         }
         else if((arr[L]>arr[F])&&(arr[L]<arr[P])) {
             swap(&arr[L], &arr[P]);
-//            std::cout << "\nredefined pivot:";
-//            std::cout << "\nF-" << arr[F] << " P-" << arr[P] <<" L-" << arr[L];
-//            print_ar(arr,SIZE);
         }
         else
-//            std::cout << "\n\npivot remains";
         do{
-//            std::cout << "\nruning cycle " << " F/arr[" << F << "]/" <<arr[F]<< " L/arr[" << L<<"]/"<<arr[L];
             while (arr[F] < arr[P]) { ++F; }
             while (arr[L] > arr[P]) { --L; }
-//            std::cout << "\nstop: F/arr["<<F<<"]/"<<arr[F]<<" > P/arr["<<P<<"]/"<<arr[P]<<" > L/arr["<<L<<"]/"<<arr[L];
             if (arr[F] >= arr[L]) {
-//                std::cout << "\nswapping F/arr["<<F<<"]/"<<arr[F]<<" <-> L/arr["<<L<<"]/"<< arr[L];
                 swap(&arr[F], &arr[L]);
-//                std::cout << "\n swapped F/arr["<<F<<"]-"<<arr[F]<<"     L/arr["<<L<<"]-"<<arr[L];
-//                print_ar(arr,SIZE);
                 F++;
                 L--;
-
-//                std::cout << "\nF++ -> arr["<<F<<"]/"<<arr[F]<<"\nL-- -> arr["<<L<<"]/"<<arr[L];
             }
         } while (arr[F] < arr[L]);
-
-//    printf("\nPartition stop -> first-%d pivot-%d last-%d", arr[F], arr[P], arr[L]);
-//    print_ar(arr,SIZE);
         if (F < last)
             Qsort(arr, F, last);
         if (L > first)
             Qsort(arr, first, L);
+    }
+}
+
+void Bsort(int *arr, int len)
+{
+    const int rows = 10; // количество блоков
+    int depth = len; // величина блока
+    int set[rows][depth+1];
+    for (int i = 0; i < len; ++i)
+        set[i][depth] = 0; //обнуление последнего элемента - счетчика
+
+    for (int dig = 1; dig < 1000000 ; dig *=10) // проход по разрядам
+    {
+        for (int i = 0; i < len; ++i) {
+            if(arr[i]%2 == 0)  // проверка четности
+            {
+                int bucket = (arr[i]/dig)%rows;       // /dig - определение разряда; %rows - индексация по блокам
+                set[bucket][set[bucket][depth]++] = arr[i];
+                // значение элемента arr[i] присваивается элементу с индексом
+                // равным значению, на которое ссылается set[bucket][depth],
+                // (т.е. на счетчик), т.о. это ссылка на значение последнего
+                // элемента блока, которое мы используем для указания позиции
+                // в этом же блоке, куда нужно записать число.
+                // Затем значение set[bucket][depth] увеличивается на 1.
+                arr[i] = -1; // "сброс" значения в оригинальном массиве для последующей перезаписи
+            }
+        }
+//        std::cout << "\n";
+//        for (int i = 0; i < len; ++i) {
+//            std::cout << arr[i] << ", ";
+        
+        int itr = 0;
+        for (int i = 0; i < rows; ++i) //перебор блоков
+        {
+            for (int j = 0; j < set[i][depth]; ++j) //проход по блоку
+            {
+                while(arr[itr] != -1) //пропуск нечетных чисел
+                    itr++;
+                arr[itr] = set[i][j];
+            }
+            set[i][depth] = 0; // обнуление счетчика для избежания повторных проходов
+        }
     }
 }
